0abf63eab7405f42aaa3ea9191ae7c8dba014599
sigi_flutter <sigi_flutter@example.com>
2021-03-31 18:07:04 +0500

    created text theme

--- lib/ui/values/styles.dart
+++ lib/ui/values/styles.dart
@@ -4,14 +4,33 @@ import 'package:hero_bear_driver/ui/values/values.dart';
 class Styles {
   static final appTheme = _baseTheme.copyWith(
     cardTheme: _baseTheme.cardTheme.copyWith(
-      margin: EdgeInsets.zero,
+        margin: EdgeInsets.zero,
+      ),
+      iconTheme: _baseTheme.iconTheme.copyWith(
+        color: _colorScheme.onBackground,
+      ),
+      textTheme: _baseTextTheme,
+      accentTextTheme: _baseTextTheme.apply(
+        bodyColor: _colorScheme.secondary,
+        displayColor: _colorScheme.secondary,
+      ));
+
+  static final _baseTextTheme = _baseTheme.textTheme.copyWith(
+    headline4: TextStyle(
+      fontSize: 18,
     ),
-    iconTheme: _baseTheme.iconTheme.copyWith(
-      color: _colorScheme.onBackground,
+    headline5: TextStyle(
+      fontSize: 16,
+    ),
+    headline6: TextStyle(
+      fontSize: 14,
     ),
   );
 
-  static final _baseTheme = ThemeData.from(colorScheme: _colorScheme);
+  static final _baseTheme = ThemeData.from(
+    colorScheme: _colorScheme,
+    textTheme: Typography.material2018().black,
+  );
 
   static const _colorScheme = ColorScheme.light(
     primary: MyColors.yellow400,
