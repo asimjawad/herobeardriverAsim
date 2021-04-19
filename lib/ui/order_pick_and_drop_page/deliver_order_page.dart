0d1d0b0631705432bc31d2632f3542ec95670c3c
sigi_flutter_2 <sigiflutter2@example.com>
2021-04-19 11:19:03 +0500

    added the polylines to the pages

--- lib/ui/order_pick_and_drop_page/deliver_order_page.dart
+++ lib/ui/order_pick_and_drop_page/deliver_order_page.dart
@@ -1,24 +1,120 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hero_bear_driver/data/app_bloc.dart';
+import 'package:hero_bear_driver/data/map_client.dart';
 import 'package:hero_bear_driver/ui/home/home_page.dart';
 import 'package:hero_bear_driver/ui/order_pick_and_drop_page/deliver_photo_and_confirm_dialog.dart';
 import 'package:hero_bear_driver/ui/order_pick_and_drop_page/pending_orders_page.dart';
 import 'package:hero_bear_driver/ui/order_pick_and_drop_page/slider_widget.dart';
 import 'package:hero_bear_driver/ui/values/values.dart';
 import 'package:hero_bear_driver/ui/widgets/show_full_line_widget.dart';
+import 'package:hero_bear_driver/util/map_util.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class DeliverOrderPage extends StatelessWidget {
-  final double _iconSize = 25;
-  final double _iconSizeS = 20;
-  final double _iconSizeL = 30;
+class DeliverOrderPage extends StatefulWidget {
   static const double _rowV = 20;
+
+  @override
+  _DeliverOrderPageState createState() => _DeliverOrderPageState();
+}
+
+class _DeliverOrderPageState extends State<DeliverOrderPage> {
+  final double _iconSize = 25;
+
+  final double _iconSizeS = 20;
+
+  final double _iconSizeL = 30;
+
   final _appBloc = Get.find<AppBloc>();
+
   File? imageSelected;
+
+  final _mapClient = MapClient();
+
+  final Set<Polyline> _polyline = {};
+
+  List<LatLng> polylineCoordinates = [];
+
+  final Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<List<LatLng>> getPolyline() async {
+    LatLng origin = await currentUserLocation();
+    LatLng destination = await getDestination();
+    var data = await _mapClient.getPolyline(origin, destination);
+    return data;
+  }
+
+  //getting current user location
+  Future<LatLng> currentUserLocation() async {
+    final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    LatLng _origin = LatLng(position.latitude, position.longitude);
+    return _origin;
+  }
+
+//getting destination location
+  LatLng getDestination() {
+    LatLng _destination = LatLng(
+        double.parse(_appBloc.orderDetailsModel.data!.orders[0].dLat),
+        double.parse(_appBloc.orderDetailsModel.data!.orders[0].dLng));
+
+    return _destination;
+  }
+
+  void _onMapCreated(GoogleMapController controllerParam) async {
+    List<LatLng> latlng = await getPolyline();
+    LatLng getOrigin = await currentUserLocation();
+    final _icon = await MapUtil.getBitmapDescriptor(
+        context, MyImgs.mapPin, Dimens.sizeMapPin);
+    setState(() {
+      //adding markers for user location and destination
+      _markers.add(Marker(
+        markerId: MarkerId('currentUserMarker'),
+        position: getOrigin,
+        icon: _icon,
+      ));
+      _markers.add(Marker(
+        markerId: MarkerId('desUserMarker'),
+        position: getDestination(),
+        icon: _icon,
+      ));
+
+      // adding polyines in list
+      _polyline.add(Polyline(
+        polylineId: PolylineId('poly'),
+        visible: true,
+        points: latlng,
+        width: 5,
+        color: Theme.of(context).colorScheme.primary,
+      ));
+    });
+
+    LatLngBounds bound;
+    bound = MapUtil.getLatLngBounds(getOrigin, getDestination());
+
+    CameraUpdate u2 = CameraUpdate.newLatLngBounds(bound, 50);
+    check(u2, controllerParam);
+  }
+
+  void check(CameraUpdate u, GoogleMapController c) async {
+    c.animateCamera(u);
+    c.moveCamera(u);
+    LatLngBounds l1 = await c.getVisibleRegion();
+    LatLngBounds l2 = await c.getVisibleRegion();
+    if (l1.southwest.latitude == -90 || l2.southwest.latitude == -90) {
+      check(u, c);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +144,14 @@ class DeliverOrderPage extends StatelessWidget {
         children: [
           GoogleMap(
             initialCameraPosition: CameraPosition(
-              target: LatLng(0, 0),
+              target: LatLng(
+                  double.parse(_appBloc.orderDetailsModel.data!.orders[0].dLat),
+                  double.parse(
+                      _appBloc.orderDetailsModel.data!.orders[0].dLng)),
             ),
+            polylines: _polyline,
+            markers: _markers,
+            onMapCreated: _onMapCreated,
             myLocationButtonEnabled: false,
             zoomControlsEnabled: false,
           ),
@@ -70,11 +172,15 @@ class DeliverOrderPage extends StatelessWidget {
                       children: [
                         Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children:[
+                            children: [
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(Strings.deliverFor,style: Styles.appTheme.textTheme.bodyText1?.copyWith(color: Colors.black54),),
+                                  Text(
+                                    Strings.deliverFor,
+                                    style: Styles.appTheme.textTheme.bodyText1
+                                        ?.copyWith(color: Colors.black54),
+                                  ),
                                   Padding(
                                     padding: const EdgeInsets.symmetric(
                                         vertical: Dimens.insetS),
@@ -89,20 +195,26 @@ class DeliverOrderPage extends StatelessWidget {
                                 ],
                               ),
                               GestureDetector(
-                                onTap: (){
+                                onTap: () {
                                   _makePhoneCall(
                                       'tel:${_appBloc.orderDetailsModel.data!.orders[0].user.phone}');
                                 },
                                 child: Container(
-                                  child:
-                                  Center(child: Icon(Icons.phone,size: _iconSizeL,color: Colors.black54,),),
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.phone,
+                                      size: _iconSizeL,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ]
-                        ),
+                            ]),
                         ShowlineFull(widthMax: true, color: Colors.black54),
                         Padding(
-                          padding: const EdgeInsets.symmetric(vertical: Dimens.insetS,),
+                          padding: const EdgeInsets.symmetric(
+                            vertical: Dimens.insetS,
+                          ),
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
@@ -188,13 +300,19 @@ class DeliverOrderPage extends StatelessWidget {
                         ),
                         ShowlineFull(widthMax: true, color: Colors.black54),
                         Padding(
-                          padding: const EdgeInsets.symmetric(vertical: Dimens.insetS),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: Dimens.insetS),
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Padding(
-                                padding: const EdgeInsets.only(right: Dimens.insetS),
-                                child: Icon(Icons.message,size: _iconSize,color: Colors.grey,),
+                                padding:
+                                    const EdgeInsets.only(right: Dimens.insetS),
+                                child: Icon(
+                                  Icons.message,
+                                  size: _iconSize,
+                                  color: Colors.grey,
+                                ),
                               ),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -216,21 +334,33 @@ class DeliverOrderPage extends StatelessWidget {
                             ],
                           ),
                         ),
-                        ShowlineFull(color: Colors.black54,widthMax: true),
+                        ShowlineFull(color: Colors.black54, widthMax: true),
                         Padding(
-                          padding: const EdgeInsets.symmetric(vertical: Dimens.insetS),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: Dimens.insetS),
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Padding(
-                                padding: const EdgeInsets.only(right: Dimens.insetS),
-                                child: Icon(Icons.cloud_download,size: _iconSize,color: Colors.grey,),
+                                padding:
+                                    const EdgeInsets.only(right: Dimens.insetS),
+                                child: Icon(
+                                  Icons.cloud_download,
+                                  size: _iconSize,
+                                  color: Colors.grey,
+                                ),
                               ),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(Strings.totalItem,style: Styles.appTheme.textTheme.bodyText1?.copyWith(color: Colors.black54,fontWeight: FontWeight.w800)),
-                                  Text('items',style: Styles.appTheme.textTheme.bodyText2?.copyWith(color: Colors.black54)),
+                                  Text(Strings.totalItem,
+                                      style: Styles.appTheme.textTheme.bodyText1
+                                          ?.copyWith(
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.w800)),
+                                  Text('items',
+                                      style: Styles.appTheme.textTheme.bodyText2
+                                          ?.copyWith(color: Colors.black54)),
                                 ],
                               ),
                             ],
@@ -254,17 +384,23 @@ class DeliverOrderPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(Dimens.insetM),
             child: Column(
-
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Stack(
                   children: [
-                    SliderWidget(func:()=> OrderDeliveredDialog(context),),
-                    Padding(
-                      padding: const EdgeInsets.only(top: _rowV),
-                      child: Center(child: Text(Strings.iHaveArrivedAtCustomer,style: Styles.appTheme.textTheme.bodyText1?.copyWith(color: Colors.white,fontWeight: FontWeight.w700),)),
+                    SliderWidget(
+                      func: () => OrderDeliveredDialog(context),
                     ),
-
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: DeliverOrderPage._rowV),
+                      child: Center(
+                          child: Text(
+                        Strings.iHaveArrivedAtCustomer,
+                        style: Styles.appTheme.textTheme.bodyText1?.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.w700),
+                      )),
+                    ),
                   ],
                 ),
               ],
